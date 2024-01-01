a806e4af9aa1f665cb241531e3b22b94b87c95e6
Vociferous <43530387+Vociferous121@users.noreply.github.com>
2024-01-01 07:38:48 -0500

    rotation mode added

--- scripts/script_rotation.lua
+++ scripts/script_rotation.lua
@@ -4,15 +4,52 @@ script_rotation = {
 	waitTimer = GetTimeEX(),
 	timer = GetTimeEX(),
 	isSetup = false,
+	useMana = true,
+	restHp = 60,
+	restMana = 60,
+	eatHealth = 60,
+	drinkMana = 60,
+	useRestFeature = false,
+	
 }
 
 function script_rotation:setup()
+	
+	SetPVE(true);
+	SetAutoLoot();
+	self.waitTimer = GetTimeEX();
+
+	-- Classes that doesn't use mana
+	local class, classFileName = UnitClass("player");
+	if (strfind("Warrior", class) or strfind("Rogue", class)) then self.useMana = false; self.restMana = 0; end
+
+	DEFAULT_CHAT_FRAME:AddMessage('script_rotation: loaded...');
+
+	if (script_grind.restHp == 0) then self.eatHealth = script_grind.restHp; end
+	if (script_grind.restMana == 0) then self.drinkMana = script_grind.restMana; end
+
+
+	self.isSetup = true;
+	
 
 end
 
 function script_rotation:run()
 
-	script_rotation:menu();
+	script_grind.shouldRest = false;
+	script_grind:restOff();
+
+	if (not self.isSetup) then
+		script_rotation:setup();
+	end
+
+	if (NewWindow("Rotation", 100, 100)) then
+		script_rotation:menu();
+	end
+
+	if (self.pause) then
+		return;
+	end
 
 	-- Check: wait for timer
 	if(self.waitTimer > GetTimeEX()) then return; end
@@ -20,17 +57,17 @@ function script_rotation:run()
 
 	if(GetTarget() ~= 0) then
 		RunCombatScript(GetTarget());
-	else
+	elseif (self.useRestFeature) then
 		RunRestScript();
 	end
+
+	
 end
 
 
 function script_rotation:menu()
-	
-	if (NewWindow("Rotation", 300, 200)) then
-	end
-	
+
+	wasClicked, self.useRestFeature = Checkbox("Stop And Rest After Combat", self.useRestFeature);
 
 	local wasClicked = false;
 	if (script_rotation.pause) then
@@ -75,4 +112,14 @@ function script_rotation:menu()
 		script_shaman:menu();
 	end
 
+	if (CollapsingHeader("[Rest options")) then
+		wasClicked, script_rotation.useMana = Checkbox("Class Uses Mana", script_rotation.useMana);
+		script_rotation.restHp = SliderInt("Eat percent", 1, 99, script_rotation.restHp);
+		if (script_rotation.useMana) then script_rotation.restMana = SliderInt("Drink percent", 1, 99, script_rotation.restMana); 
+			Text("Use potions (when in combat):");
+			script_rotation.potHp = SliderInt("HP percent", 1, 99, script_rotation.potHp);
+			if (script_rotation.useMana) then script_rotation.potMana = SliderInt("Mana percent", 1, 99, script_rotation.potMana);
+			end
+		end
+	end
 end
